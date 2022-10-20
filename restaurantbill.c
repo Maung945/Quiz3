0f10c8c52b6742554a61733df5171197ebf4fac0
Maung945 <myoaung@cpp.edu>
2022-10-20 14:17:30 -0700

    First Commit

---
+++ restaurantbill.c
@@ -0,0 +1,7 @@
+/*
+* Myo Aung, CS26000 - System Programming 
+* Declaration for items will be made
+* Then, create methods/functions that calculate Tax and Tip
+* Then values will be passed into the calulateTax() and calculateTip()
+* Finally, prints out total value to the screen
+*/

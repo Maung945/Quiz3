db2179be1b8a04f7d11477071cf6f1f5bade28f9
Maung945 <myoaung@cpp.edu>
2022-10-20 23:42:32 -0700

    Second Push- Project Complete

--- restaurantbill.c
+++ restaurantbill.c
@@ -1,7 +1,215 @@
 /*
-* Myo Aung, CS26000 - System Programming 
-* Declaration for items will be made
-* Then, create methods/functions that calculate Tax and Tip
-* Then values will be passed into the calulateTax() and calculateTip()
-* Finally, prints out total value to the screen
-*/
+ * Myo Aung, CS26000 - System Programming
+ * 
+ * Implentation of pointers and pointer functions learned class.
+ * Create a Menu table using struct
+ * ptr - points to the table and iterate through it to search the target
+ * Create methods/functions that calculate Tax and Tip
+ * printGrandTotal() will call calulateTax() and calculateTip()
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+#define TAX 0.095                                                       // Macro for Tax Percentage
+
+typedef struct                                                          // Struct
+{
+    int item;
+    char *itemName;
+    double price;
+} Menu, *PtrToMenu;
+
+typedef const Menu *PtrtoItems;
+
+Menu MenuTable[] = {
+    {1, "Salad", 9.95}, {2, "Soup", 4.55}, {3, "Sandwich", 13.25}, {4, "Pizza", 22.35}};
+
+const int MenuTableEntries = sizeof(MenuTable) / sizeof(MenuTable[0]);  // Size of the array
+
+PtrToMenu display(PtrtoItems ptr, int tableSize)                        // Pointer return funcion 
+{
+    const PtrtoItems endPtr = ptr + tableSize;                          // End-Pointer is set for the loop to stop
+    for (; ptr < endPtr; ptr++)
+    {
+        printf("%d) %s: $%1.2f\n", ptr->item, ptr->itemName, ptr->price);
+    }
+}
+
+PtrToMenu searchItem(PtrtoItems ptr, int tableSize, int target)         // Pointer return function for search item
+{
+    const PtrtoItems endPtr = ptr + tableSize;
+    for (; ptr < endPtr; ptr++)
+    {
+        if (ptr->item == target)                                        // If ptr pointer points at target
+            return (PtrToMenu)ptr;                                      // Return that ptr pointer
+    }
+    return NULL;
+}
+
+double calculateOneItems(PtrToMenu ptr1)                                // Calculte for One Item
+{
+    return (ptr1->price);
+}
+
+double calculateTwoItems(PtrToMenu ptr1, PtrToMenu ptr2)                // Calculte for One Item
+{
+    return (ptr1->price + ptr2->price);
+}
+
+double calculateThreeItems(PtrToMenu ptr1, PtrToMenu ptr2, PtrToMenu ptr3)
+{
+    return (ptr1->price + ptr2->price + ptr3->price);                   // Calculte for One Item
+}
+
+double calculateFourItems(PtrToMenu ptr1, PtrToMenu ptr2, PtrToMenu ptr3, PtrToMenu ptr4)
+{
+    return (ptr1->price + ptr2->price + ptr3->price + ptr4->price);     // Calculte for One Item
+}
+
+double calculateTip(double total, int tipPercentage)                    // Calculate Tips
+{
+    return (total * (tipPercentage * 0.01));
+}
+
+double calculateTax(double total)                                       // Calculate Tax
+{
+    return (total * TAX);
+}
+
+void printGrandTotal(double total)                                      // Void function for display
+{
+    double tip;
+    double grandTotal;
+    printf("Enter Tip %%\t\t    : ");
+    scanf("%lf", &tip);
+    printf("Total Tip\t\t    : $%1.2f\n", calculateTip(total, tip));
+    printf("Tax 9.5%%\t\t    : $%1.2f\n", calculateTax(total));
+    printf("-------------------------------------\n");
+    grandTotal = total + calculateTip(total, tip) + calculateTax(total);
+    printf("Your Grand Total\t    : $%1.2f\n", grandTotal);
+    printf("-------------------------------------\n\n");
+}
+
+int main(void)                                                          // Main function
+{
+    int itemOne, itemTwo, itemThree, itemFour;
+    int itemCount;
+    printf("\n\n     -------------\n");
+
+    printf("     Today's Menu\n");
+    printf("     -------------\n");
+
+    PtrToMenu display(const Menu table[], int tableSize);
+
+    display(MenuTable, MenuTableEntries);
+    printf("--------------------------\n");
+    printf("How many items selecting: ");
+    scanf("%d", &itemCount);
+    printf("--------------------------\n");
+
+    PtrToMenu searchItem(const Menu table[], int tableSize, int target);
+    
+    double total;
+    
+    switch (itemCount)
+    {
+    case 1:
+        printf("Choose First Item: ");
+        scanf("%d", &itemOne);
+        printf("--------------------------\n");
+        PtrToMenu matchPtr;
+        matchPtr = searchItem(MenuTable, MenuTableEntries, itemOne);
+        printf("Item Selected: %s is $%1.2f\n", matchPtr->itemName, matchPtr->price);
+        printf("--------------------------\n");
+        total = calculateOneItems(matchPtr);
+
+        printf("Sub-Total for ONE item is   : $%1.2f\n", total);
+        printGrandTotal(total);
+        break;
+
+    case 2:
+        printf("Choose First Item Number: ");
+        scanf("%d", &itemOne);
+        printf("Choose Second Item Number: ");
+        scanf("%d", &itemTwo);
+        printf("--------------------------\n");
+        PtrToMenu matchPtr1;
+        matchPtr1 = searchItem(MenuTable, MenuTableEntries, itemOne);
+        printf("Item 1: %s is $%1.2f\n", matchPtr1->itemName, matchPtr1->price);
+
+        PtrToMenu matchPtr2;
+        matchPtr2 = searchItem(MenuTable, MenuTableEntries, itemTwo);
+        printf("Item 2: %s is $%1.2f\n", matchPtr2->itemName, matchPtr2->price);
+        printf("--------------------------\n");
+        total = calculateTwoItems(matchPtr1, matchPtr2);
+        printf("Sub-Total for TWO items is  : $%1.2f\n", total);
+        printGrandTotal(total);
+        break;
+    case 3:
+
+        printf("Choose First Item Number: ");
+        scanf("%d", &itemOne);
+        printf("Choose Second Item Number: ");
+        scanf("%d", &itemTwo);
+        printf("Choose Third Item Number: ");
+        scanf("%d", &itemThree);
+        printf("--------------------------\n");
+
+        PtrToMenu matchPtrOne;
+        matchPtrOne = searchItem(MenuTable, MenuTableEntries, itemOne);
+        printf("Item 1: %s is $%1.2f\n", matchPtrOne->itemName, matchPtrOne->price);
+
+        PtrToMenu matchPtrTwo;
+        matchPtrTwo = searchItem(MenuTable, MenuTableEntries, itemTwo);
+        printf("Item 2: %s is $%1.2f\n", matchPtrTwo->itemName, matchPtrTwo->price);
+
+        PtrToMenu matchPtrThree;
+        matchPtrThree = searchItem(MenuTable, MenuTableEntries, itemThree);
+        printf("Item 3: %s is $%1.2f\n", matchPtrThree->itemName, matchPtrThree->price);
+        printf("--------------------------\n");
+        total = calculateThreeItems(matchPtrOne, matchPtrTwo, matchPtrThree);
+        printf("Sub-Total for THREE items is: $%1.2f\n", total);
+
+        printGrandTotal(total);
+        break;
+
+    case 4:
+        printf("Choose First Item Number: ");
+        scanf("%d", &itemOne);
+        printf("Choose Second Item Number: ");
+        scanf("%d", &itemTwo);
+        printf("Choose Third Item Number: ");
+        scanf("%d", &itemThree);
+        printf("Choose Fourth Item Number: ");
+        scanf("%d", &itemFour);
+        printf("--------------------------\n");
+
+        PtrToMenu ptrOne;
+        ptrOne = searchItem(MenuTable, MenuTableEntries, itemOne);
+        printf("Item 1: %s is $%1.2f\n", ptrOne->itemName, ptrOne->price);
+
+        PtrToMenu ptrTwo;
+        ptrTwo = searchItem(MenuTable, MenuTableEntries, itemTwo);
+        printf("Item 2: %s is $%1.2f\n", ptrTwo->itemName, ptrTwo->price);
+
+        PtrToMenu ptrThree;
+        ptrThree = searchItem(MenuTable, MenuTableEntries, itemThree);
+        printf("Item 3: %s is $%1.2f\n", ptrThree->itemName, ptrThree->price);
+
+        PtrToMenu ptrFour;
+        ptrFour = searchItem(MenuTable, MenuTableEntries, itemFour);
+        printf("Item 4: %s is $%1.2f\n", ptrFour->itemName, ptrFour->price);
+
+        printf("--------------------------\n");
+        total = calculateFourItems(ptrOne, ptrTwo, ptrThree, ptrFour);
+        printf("Sub-Total for FOUR items is : $%1.2f\n", total);
+
+        printGrandTotal(total);
+        break;
+
+    default:
+        printf("Enter a valid number\n");
+    }
+
+    return EXIT_SUCCESS;
+}
